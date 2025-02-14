156f737e788b1a3d5a29e052cd4afa7f5e383354
Petrus Brink <peterjbrink@gmail.com>
2025-02-14 15:39:48 +1100

    Added get endpoint.

--- pages/api/store-search.js
+++ pages/api/store-search.js
@@ -6,6 +6,10 @@ const {
 } = require("../../src/lib/ioredis");
 
 export default async function handler(req, res) {
+  if (req.method === "GET") {
+    return res.status(200).json({ message: "You hit this endpoint" });
+  }
+
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   } else {
