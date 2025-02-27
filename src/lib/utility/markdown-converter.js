6fcee97f33d0eb0b4ce38705745bca6dfee5b77f
Petrus Brink <peterjbrink@gmail.com>
2025-02-27 14:31:15 +1100

    Added syntax highlighting support for more languages.

--- src/lib/utility/markdown-converter.js
+++ src/lib/utility/markdown-converter.js
@@ -6,6 +6,12 @@ import Prism from "prismjs";
 import "prismjs/components/prism-dart";
 import "prismjs/components/prism-java";
 import "prismjs/components/prism-python";
+import "prismjs/components/prism-go";
+import "prismjs/components/prism-ruby";
+import "prismjs/components/prism-swift";
+import "prismjs/components/prism-typescript";
+import "prismjs/components/prism-css";
+import "prismjs/components/prism-markup";
 
 const MarkdownConverter = React.memo(({ input }) => {
   if (!input) {
