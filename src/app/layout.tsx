c66e091ecf9cfce19025ee5c559537f95471ab26
Petrus Brink <peterjbrink@gmail.com>
2025-02-27 14:31:28 +1100

    Restructured css.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "@/styles/prism.css";
+import "@/styles/prose.css";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
